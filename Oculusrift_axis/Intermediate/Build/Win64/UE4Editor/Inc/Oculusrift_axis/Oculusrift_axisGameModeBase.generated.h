b529777b295965f6a7798fe246da6249c93f2c2f
codyowl <codyowl@gmail.com>
2018-04-13 19:06:30 +0530

    Project initialized :baby_bottle

---
+++ Oculusrift_axis/Intermediate/Build/Win64/UE4Editor/Inc/Oculusrift_axis/Oculusrift_axisGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef OCULUSRIFT_AXIS_Oculusrift_axisGameModeBase_generated_h
+#error "Oculusrift_axisGameModeBase.generated.h already included, missing '#pragma once' in Oculusrift_axisGameModeBase.h"
+#endif
+#define OCULUSRIFT_AXIS_Oculusrift_axisGameModeBase_generated_h
+
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_RPC_WRAPPERS
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAOculusrift_axisGameModeBase(); \
+	friend OCULUSRIFT_AXIS_API class UClass* Z_Construct_UClass_AOculusrift_axisGameModeBase(); \
+public: \
+	DECLARE_CLASS(AOculusrift_axisGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/Oculusrift_axis"), NO_API) \
+	DECLARE_SERIALIZER(AOculusrift_axisGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAOculusrift_axisGameModeBase(); \
+	friend OCULUSRIFT_AXIS_API class UClass* Z_Construct_UClass_AOculusrift_axisGameModeBase(); \
+public: \
+	DECLARE_CLASS(AOculusrift_axisGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/Oculusrift_axis"), NO_API) \
+	DECLARE_SERIALIZER(AOculusrift_axisGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AOculusrift_axisGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AOculusrift_axisGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AOculusrift_axisGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AOculusrift_axisGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AOculusrift_axisGameModeBase(AOculusrift_axisGameModeBase&&); \
+	NO_API AOculusrift_axisGameModeBase(const AOculusrift_axisGameModeBase&); \
+public:
+
+
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AOculusrift_axisGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AOculusrift_axisGameModeBase(AOculusrift_axisGameModeBase&&); \
+	NO_API AOculusrift_axisGameModeBase(const AOculusrift_axisGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AOculusrift_axisGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AOculusrift_axisGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AOculusrift_axisGameModeBase)
+
+
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_12_PROLOG
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_RPC_WRAPPERS \
+	Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_INCLASS \
+	Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Oculusrift_axis_Source_Oculusrift_axis_Oculusrift_axisGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
