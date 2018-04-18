205c5a4ac1fde8221748bee4b702f3bd28b2441d
codyowl <codyowl@gmail.com>
2018-04-18 19:14:17 +0530

    Camera movement c++ program under progress :rocket:

---
+++ ThirdPersonCamera/Source/ThirdPersonCamera/MyPawn.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyPawn.h"
+
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	spring = CreateDefaultSubobject<USpringArmcomponent>(TEXT("spring"));
+	camera = CreateDefaultSubobject<UCameraComponent>(TEXT("camera"));
+	mesh = CreateDefaultSubobject<UMeshComponent>(TEXT("mesh"));
+
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
