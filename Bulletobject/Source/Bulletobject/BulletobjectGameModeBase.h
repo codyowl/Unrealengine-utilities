7ca4d903c1b93765f2da28c68943edb4c231319d
codyowl <codyowl@gmail.com>
2018-04-19 19:08:58 +0530

    Bullet behaviour initialized :baby_bottle:

---
+++ Bulletobject/Source/Bulletobject/BulletobjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BulletobjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLETOBJECT_API ABulletobjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
